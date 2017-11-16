2853af44b01df29e7cad12239d1cd1d69f3873b6
Apurva404 <apurvajha90@hmail.com>
2017-11-16 14:57:23 -0800

    Changes ths style and colors

---
+++ app/src/main/java/assignment/apurva/com/justlightapp/activities/CustomerSignUpActivity.java
@@ -0,0 +1,19 @@
+package assignment.apurva.com.justlightapp.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import assignment.apurva.com.justlightapp.R;
+
+/**
+ * Created by Apurva on 11/16/2017.
+ */
+
+public class CustomerSignUpActivity  extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customersignup);
+    }
+}
+
